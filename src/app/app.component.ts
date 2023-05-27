5b32ce4d53c09a3962303aa1eaf593e4f13ad60e
LEPEGO <Leo24pedr@gmail.com>
2023-05-27 10:55:14 -0600

    Proyecto

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import mapboxgl from 'mapbox-gl';
+import { environment } from '../config';
+import { MatSelectModule } from '@angular/material/select';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Reservaciones';
+  comments: string[] = [];
+  onNewCommentAdded(comment: string) {
+    console.log('Nuevo comentario agregado:', comment);
+  }
+  ngOnInit() {
+    mapboxgl.accessToken = environment.mapbox.accessToken;
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-74.5, 40],
+      zoom: 9
+    });
+  }
+  isActive = false;
+
+  changeBackgroundColor() {
+    this.isActive = !this.isActive;
+  }
+}
---
+++ src/app/tabla/tabla.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, OnInit, OnChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-tabla',
+  templateUrl: './tabla.component.html',
+  styleUrls: ['./tabla.component.css']
+})
+export class TablaComponent implements OnInit, OnChanges {
+  @Input() registros: any[] = [];
+  registrosMostrados: any[] = [];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  ngOnChanges() {
+    if (this.registros && this.registros.length > 0) {
+      this.registrosMostrados = this.registros;
+    }
+  }
+}
