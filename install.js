2fc31ee2b858ad93bce4205a38b97ddca7969bd0
LEPEGO <Leo24pedr@gmail.com>
2023-05-29 10:20:48 -0600

    Cambios

--- install.js
+++ install.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict';//Para que se ejecute en modo estricto
 
 let deferredInstallPrompt = null;
 const installButton = document.getElementById('butIntsall');
@@ -13,8 +13,8 @@ function saveBeforeInstallPromptEvent(evt){
 
 function installPWA(evt){
     deferredInstallPrompt.prompt();
-    evt.srcElemt.setAttribute('hidden', true);
-    deferredInstallPrompt.userChoice.then((choice)=>{
+    svt.srcElemt.setAttribute('hidden', true);//Para que el boton de instalacion se oculte despues de instalarlo 
+    deferredInstallPrompt.userChoice.then((choice)=>{//Para ver si el usuario acepta o no la instalacion
         if(choice.outcome === "accepted"){
             console.log("aceptado")
         } else{
@@ -26,6 +26,8 @@ function installPWA(evt){
 
 window.addEventListener('appinstalled', logAppInstalled);
 
-function logAppInstalled(evt){
+function logAppInstalled(evt){//te muestra que se intsalo la aplicacion
     console.log("APP INSTALADA ")
 }
+
+//ng add @angular/pwa me sirve para que se pueda ver de forma offline y no se caiga la pagina 
